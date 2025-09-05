35d01b890fb3272d06e22eedf4a988cb25565d89
Chrissivert <122440126+Chrissivert@users.noreply.github.com>
2025-09-05 11:04:38 +0200

    core and one extension

---
+++ src/app/people.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-people',
+  templateUrl: './people.component.html',
+  styleUrls: ['./people.component.css']
+})
+export class PeopleComponent {
+  @Input() name!: string;
+  @Output() favourite = new EventEmitter<void>();
+
+  setFavourite() {
+    this.favourite.emit();
+  }
+}
